7271f4691d158b416a2eead2d0d3dc88d95c9078
felipevsena <fvsena@outlook.com>
2019-05-28 22:48:54 -0300

    Editando tela de produto

--- OdinPizzaria/src/view/TelaProduto.java
+++ OdinPizzaria/src/view/TelaProduto.java
@@ -1,18 +1,24 @@
 package view;
 
+import java.util.Arrays;
+
 import controller.ProdutoController;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import model.Ingrediente;
+import model.TamanhoPizza;
 
 public class TelaProduto extends Application implements EventHandler<ActionEvent>  {
 
@@ -26,6 +32,7 @@ public class TelaProduto extends Application implements EventHandler<ActionEvent
 	private Label lblNome = new Label("NOME");
 	private Label lblValor = new Label("VALOR");
 	private Label lblIngredientes  = new Label("TIPO");
+	private Label lblTamanho  = new Label("TAMANHO");
 
 	private TextField txtTipo = new TextField();
 	private TextField txtNome = new TextField();
@@ -33,13 +40,26 @@ public class TelaProduto extends Application implements EventHandler<ActionEvent
 	private TextField txtIngredientes = new TextField();
 	private TextField txtBuscar = new TextField();
 	
-	private TableView<Ingrediente> tblIngredientes = new TableView<>(); 
+	private TableView<Ingrediente> tblIngredientes = new TableView<>();
+	
+	ObservableList<String> tipoProduto = FXCollections.observableArrayList(
+				"PIZZA",
+				"BEBIDA"
+			);
+	
+	ObservableList<String> tamanhoPizza = FXCollections.observableArrayList(
+			"BROTO",
+			"MÉDIA",
+			"GRANDE"
+		);
 
+	final ComboBox cmbTipoProduto = new ComboBox(tipoProduto);
+	final ComboBox cmbTamanho = new ComboBox(tamanhoPizza);
 
 	@Override
 	public void handle(ActionEvent event) {
 		if (event.getTarget() == btnCadastrar) {
-			habilitarControles(true);
+			habilitarControles(false);
 		}
 		if (event.getTarget() == btnBuscar) {
 			//logar();
@@ -61,18 +81,20 @@ public class TelaProduto extends Application implements EventHandler<ActionEvent
 
 
 		btnCadastrar.relocate(100, 50);
-		btnBuscar.relocate(220, 50);
+		btnBuscar.relocate(220, 10);
 		lblTipo.relocate(50,100);
 		lblNome.relocate(50,150);
 		lblValor.relocate(50,200);
 		lblIngredientes.relocate(50,250);
-		txtTipo.relocate(100,100);
-		txtNome.relocate(100,150);
-		txtValor.relocate(100,200);
-		txtIngredientes.relocate(100,250);
-		txtBuscar.relocate(300,50);
-		btnGravar.relocate(100, 300);
-		btnSair.relocate(200, 300);
+		lblTamanho.relocate(50, 300);
+		cmbTipoProduto.relocate(120,100);
+		txtNome.relocate(120,150);
+		txtValor.relocate(120,200);
+		txtIngredientes.relocate(120,250);
+		cmbTamanho.relocate(120, 300);
+		txtBuscar.relocate(50,10);
+		btnGravar.relocate(120, 350);
+		btnSair.relocate(200, 350);
 
 		painel.getChildren().add(btnCadastrar);
 		painel.getChildren().add(btnBuscar);
@@ -80,7 +102,9 @@ public class TelaProduto extends Application implements EventHandler<ActionEvent
 		painel.getChildren().add(lblNome);
 		painel.getChildren().add(lblValor);
 		painel.getChildren().add(lblIngredientes);
-		painel.getChildren().add(txtTipo);
+		painel.getChildren().add(lblTamanho);
+		painel.getChildren().add(cmbTipoProduto);
+		painel.getChildren().add(cmbTamanho);
 		painel.getChildren().add(txtNome);
 		painel.getChildren().add(txtValor);
 		painel.getChildren().add(txtIngredientes);
@@ -93,6 +117,7 @@ public class TelaProduto extends Application implements EventHandler<ActionEvent
 		btnGravar.addEventFilter(ActionEvent.ACTION, this);
 		btnSair.addEventFilter(ActionEvent.ACTION, this);
 		
+		habilitarControles(true);
 		
 		stage.setTitle("PRODUTO");
 		stage.setScene(scn);
@@ -108,11 +133,12 @@ public class TelaProduto extends Application implements EventHandler<ActionEvent
 		lblNome.setDisable(habilitar);
 		lblValor.setDisable(habilitar);
 		lblIngredientes.setDisable(habilitar);
-		txtTipo.setDisable(habilitar);
+		lblTamanho.setDisable(habilitar);
+		cmbTipoProduto.setDisable(habilitar);
+		cmbTamanho.setDisable(habilitar);
 		txtNome.setDisable(habilitar);
 		txtValor.setDisable(habilitar);
 		txtIngredientes.setDisable(habilitar);
-		txtBuscar.setDisable(habilitar);
 	}
 	
 	private void fechar() {
@@ -125,6 +151,20 @@ public class TelaProduto extends Application implements EventHandler<ActionEvent
 	}
 	
 	private void InserirPizza() {
+		if(validaCamposPreenchidos()) {
+			//controller.InserirPizza(txtNome.getText(), cmbTamanho.getValue().toString(), txtIngredientes.getText(), txtNome.getText(), Float.parseFloat(txtValor.getText()));
+		}
 		//controller.InserirPizza(txtNome, tamanho, ingredientes, nome, valor);
+	}
+
+	private boolean validaCamposPreenchidos() {
+		boolean valido = true;
+		if(cmbTipoProduto.getValue()== null
+				|| txtNome.getText() == null
+				|| txtValor.getText() == null
+				|| txtIngredientes.getText() == null) {
+			valido = false;
+		}
+		return valido;
 	}
 }
